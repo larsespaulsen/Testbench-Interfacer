f0c4b11b359ceb56652c76934a3e01d68e3711a9
larsespaulsen <lars.paulsen@micronitor.com>
2016-06-16 15:14:15 +0200

    Basic interface and Serial Com port

---
+++ DNS-gyro Testbench Interfacer/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace DNS_gyro_Testbench_Interfacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_serialConnect = new System.Windows.Forms.Button();
+            this.Baudrate = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_Baudrate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_serialConnect
+            // 
+            this.bt_serialConnect.Location = new System.Drawing.Point(12, 184);
+            this.bt_serialConnect.Name = "bt_serialConnect";
+            this.bt_serialConnect.Size = new System.Drawing.Size(75, 23);
+            this.bt_serialConnect.TabIndex = 0;
+            this.bt_serialConnect.Text = "Connect";
+            this.bt_serialConnect.UseVisualStyleBackColor = true;
+            this.bt_serialConnect.Click += new System.EventHandler(this.bt_serialConnect_Click);
+            // 
+            // Baudrate
+            // 
+            this.Baudrate.Location = new System.Drawing.Point(12, 158);
+            this.Baudrate.Name = "Baudrate";
+            this.Baudrate.Size = new System.Drawing.Size(75, 20);
+            this.Baudrate.TabIndex = 1;
+            this.Baudrate.TextChanged += new System.EventHandler(this.Baudrate_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(257, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(257, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(257, 158);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(257, 184);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(257, 210);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(257, 236);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(257, 262);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(257, 288);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(257, 314);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 10;
+            // 
+            // console
+            // 
+            this.console.Location = new System.Drawing.Point(388, 44);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(499, 292);
+            this.console.TabIndex = 11;
+            this.console.Text = "";
+            // 
+            // comList
+            // 
+            this.comList.FormattingEnabled = true;
+            this.comList.Location = new System.Drawing.Point(12, 26);
+            this.comList.Name = "comList";
+            this.comList.Size = new System.Drawing.Size(75, 95);
+            this.comList.TabIndex = 12;
+            this.comList.SelectedIndexChanged += new System.EventHandler(this.comList_SelectChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // lb_Baudrate
+            // 
+            this.lb_Baudrate.AutoSize = true;
+            this.lb_Baudrate.Location = new System.Drawing.Point(12, 139);
+            this.lb_Baudrate.Name = "lb_Baudrate";
+            this.lb_Baudrate.Size = new System.Drawing.Size(50, 13);
+            this.lb_Baudrate.TabIndex = 13;
+            this.lb_Baudrate.Text = "Baudrate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 490);
+            this.Controls.Add(this.lb_Baudrate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comList);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Baudrate);
+            this.Controls.Add(this.bt_serialConnect);
+            this.Name = "Form1";
+            this.Text = "DNS-gyro Testbench Interfacer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_serialConnect;
+        private System.Windows.Forms.TextBox Baudrate;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.RichTextBox console;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox comList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_Baudrate;
+    }
+}
+
---
+++ DNS-gyro Testbench Interfacer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using SERCOM;
+using System.Timers;
+
+namespace DNS_gyro_Testbench_Interfacer
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort _serialPort;
+        static bool _continue;
+
+        private Object serialLock = new Object();
+        string serialData = " ";
+
+        public Form1()
+        {
+
+            var timer = new System.Windows.Forms.Timer();
+            timer.Tick += new EventHandler(UpdateConsole);
+            timer.Interval = 100; //100 ms
+            timer.Start();
+
+            InitializeComponent();
+            _serialPort = new SerialPort();
+            _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), "None", true);
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "One", true);
+            _serialPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "None", true);
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+            _serialPort.BaudRate = 230400;
+            _serialPort.PortName = "COM4";
+            console.Text = "This is a TextBox control.";
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comList.Items.Add(s);
+            }
+        }
+
+        private void Baudrate_TextChanged(object sender, EventArgs e)
+        {
+            _serialPort.BaudRate = Int32.Parse(Baudrate.Text);
+        }
+
+        private void comList_SelectChanged(object sender, EventArgs e)
+        {
+            _serialPort.PortName = comList.SelectedItem.ToString();
+        }
+
+        private void UpdateConsole(object sender, EventArgs e)
+        {
+            if (!_continue)
+            {
+                console.Text = "Port: " + _serialPort.PortName
+                + "\nBaudrate: " + _serialPort.BaudRate.ToString()
+                + "\nParity: " + _serialPort.Parity
+                + "\nDataBits: " + _serialPort.DataBits.ToString()
+                + "\nStopBits: " + _serialPort.StopBits
+                + "\nHandshake: " + _serialPort.Handshake;
+            }
+            else
+            {
+                lock (serialLock) { console.Text = serialData; }
+            }
+        }
+
+        private void bt_serialConnect_Click(object sender, EventArgs e)
+        {
+            string name;
+            string message;
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+            Thread readThread = new Thread(serial_Read);
+            if (!_continue)
+            {
+                _serialPort.Open();
+                _continue = true;
+                readThread.Start();
+                bt_serialConnect.Text = "Disconnect";
+            }
+            else
+            {
+                _continue = false;
+                readThread.Join();
+                _serialPort.Close();
+                bt_serialConnect.Text = "Connect";
+            }
+        }
+
+        private void serial_Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    lock (serialLock) { serialData = message.ToString(); }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+    }
+}
+
